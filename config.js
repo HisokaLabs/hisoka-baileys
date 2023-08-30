6bffb0170545a254d133b73847ed1918c7b7c113
Lanxz09 <143253773+Lanxz09@users.noreply.github.com>
2023-08-31 02:18:38 +0800

    Update config.js

--- config.js
+++ config.js
@@ -47,7 +47,7 @@ export default {
       public: true,
       antiCall: true, // reject call
       database: "database.json", // End .json when using JSON database or use Mongo URI
-      owner: ["6288292024190"], // set owner number on here
+      owner: ["6283103795763"], // set owner number on here
       sessionName: "session", // for name session
       prefix: /^[°•π÷×¶∆£¢€¥®™+✓_=|/~!?@#%^&.©^]/i,
       pairingNumber: "" // Example Input : 62xxx
